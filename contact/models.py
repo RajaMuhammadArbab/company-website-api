7edec36d33fd78397f3500468ce5acb987168bd4
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-09-04 11:06:31 +0500

    Create models.py

---
+++ contact/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-submitted_at']
+
+    def __str__(self):
+        return f"Contact from {self.name} - {self.submitted_at.strftime('%Y-%m-%d %H:%M')}"
